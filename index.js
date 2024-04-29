9dec191ff56db416bd64002314a85267e268e412
PrasannaAdabala <prasannaadabala385@gmail.com>
2024-04-29 17:57:09 +0530

    file

---
+++ index.js
@@ -0,0 +1,104 @@
+// 1) Write a program that takes number as input, print the sum of odd digits in the given number
+// input: 54213
+// output: 9
+// const input=require("prompt-sync")();
+// let num=input("enter a number=")
+// let sum=0;
+// for(i=0;i<num.length;i++){
+//     if(num[i]%2!==0){
+//         sum=sum+Number(num[i])
+//     }
+   
+// }
+// console.log(sum)
+
+
+// Write a program to print the sum of even digits in the given numbers
+// input:542
+// output: 6
+// const input=require("prompt-sync")();
+// let num=input("enter a number=")
+// let sum=0;
+// for(i=0;i<num.length;i++){
+//     if(num[i]%2==0){
+//         sum=sum+Number(num[i])
+//     }
+   
+// }
+// console.log(sum)
+
+// Write a program to print the differnc between even sum and odd sum in the given number.
+// input: 16534
+// output:1
+// const input=require("prompt-sync")();
+// let num=input("enter a number=")
+// let sum1=0;
+// let sum2=0;
+// for(i=0;i<num.length;i++){
+//     if(num[i]%2==0){
+//         sum1=sum1+Number(num[i])
+//     }
+//     else if(num[i]%2!==0){
+//         sum2=sum2+Number(num[i])
+//     }
+   
+// }
+// console.log(sum1-sum2)
+
+// Write a program to print the largest sum between odd sum and even sum.
+// input: 9474
+// output: Odd sum is bigger
+// input: 1658
+// output: Even sum is bigger
+// const input=require("prompt-sync")();
+// let num=input("enter a number=")
+// let sum1=0;
+// let sum2=0;
+// for(i=0;i<num.length;i++){
+//     if(num[i]%2==0){
+//         sum1=sum1+Number(num[i])
+//     }
+//     else {
+//         sum2=sum2+Number(num[i])
+//     }
+
+// }
+// if(sum1<sum2){
+//     console.log("odd is bigger")
+// }
+// else {
+//     console.log("even sum is bigger")
+// }
+
+// const input=require("prompt-sync")();
+// let num=input("enter a number=")
+// if(num>1){
+// count=0;
+
+
+
+//  for(i=2;i<num;i++){
+//     if(num%i==0){
+//         count++
+//         console.log("not a prime")
+//         break;
+//     }
+
+//  }
+//  if(count==0){
+//     console.log("prime number")
+//  }
+// }
+//  else {
+//     console.log("1 is neither prime nor composite")
+//  }
+
+
+
+
+
+
+
+
+
+
